136d33c7bc21344324c4b74408ec5c6948178235
sebascontents <157534242+sebascontents@users.noreply.github.com>
2024-02-20 19:11:51 -0300

    Update and rename home.js to header.php
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

---
+++ header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-center">
+    </button>
+    <a class="navbar-brand" href="/">Sebas Contents <span class="badge badge-primary">Beta</span></a>
+
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" href="/">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/about">About us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/contents">Contenidos</a>
+  </li>
+</ul>
+</div>
+</nav>
