55789a87a8844234de2d6b1f0298ea97675bd684
sebascontents <157534242+sebascontents@users.noreply.github.com>
2024-04-01 13:17:32 -0300

    Update scripts.js
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,44 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+
+function todoList() {
+
+// creating a variable from the username ID
+	var userName = document.getElementById ("username").value; 
+
+// creating a variable from the comment ID
+	var userComment = document.getElementById("comment").value;
+
+// text entered in the username box. It would show "Username: Plus whatever the user wrote"
+	var userNameText = document.createTextNode("Username:" + userName)
+
+// text entered in the username box. It would show "comment: Plus whatever the user wrote"
+	var commentText = document.createTextNode("Comment:" + userComment)
+
+// creating a new list item so that the text that a user writes will go here
+
+	var newListItem = document.createElement("li")
+
+// giving a class name to the newListItem
+	newListItem.className = "list-group-item"
+
+// creating a new H# element where the username text will show
+	var newUserName = document.createElement ("h3")
+
+// adding the userNameText variable to the newUserName element
+	newUserName.appendChild(userNameText)
+
+// creating a P element for the contents in the comment box to go
+	var newComment = document.createElement ("p")
+
+// adding the commentText ID variable to the newComment (P) element
+	newComment.appendChild(commentText)
+
+	newListItem.appendChild(newUserName)
+	newListItem.appendChild(newComment)
+
+	document.getElementById ("commentList").appendChild(newListItem)
+
+
+
+};
+
