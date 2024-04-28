4926edb978c1e101dfddb2177144ebd2b1fd3e93
sebascontents <157534242+sebascontents@users.noreply.github.com>
2024-04-28 09:41:32 -0300

    Delete js/comment.js
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

--- js/comment.js
+++
@@ -1,8 +0,0 @@
-<script src="https://utteranc.es/client.js"
-        repo="sebascontents/sebascontents.github.io"
-        issue-term="pathname"
-        label="Commets"
-        theme="github-dark"
-        crossorigin="anonymous"
-        async>
-</script>
