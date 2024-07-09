9b8ab05667848ae0e8015e9f5e7f662d7493a71f
sebascontents <157534242+sebascontents@users.noreply.github.com>
2024-07-09 15:03:36 -0300

    Update guest.js
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

--- js/guest.js
+++ js/guest.js
@@ -1,1 +1,7 @@
-$(document).ready(function(){$('.follow_d, .stardiv').click(function(){if(window.confirm("Solo usuarios registrados pueden utilizar esta funciÃ³n.")){location.href="/register";}});});
+$(document).ready(function () {
+    $('.follow_d, .stardiv').click(function () {
+        if (window.confirm("Solo usuarios registrados pueden utilizar esta funciÃ³n.")) {
+            location.href = "/register";
+        }
+    });
+});
