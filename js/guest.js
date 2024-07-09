c3e645796576ecd4af93292a5798ea799c1665c5
sebascontents <157534242+sebascontents@users.noreply.github.com>
2024-07-09 13:23:10 -0300

    Create guest.js
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

---
+++ js/guest.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.follow_d, .stardiv').click(function(){if(window.confirm("Solo usuarios registrados pueden utilizar esta funciÃ³n.")){location.href="/register";}});});
