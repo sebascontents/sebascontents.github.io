c7daee75d681ba724e0c17f60b757319086b93e4
sebascontents <157534242+sebascontents@users.noreply.github.com>
2024-07-09 15:30:39 -0300

    Delete js/guest.js
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

--- js/guest.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.follow_d, .stardiv').click(function () {
-        if (window.confirm("Solo usuarios registrados pueden utilizar esta funciÃ³n.")) {
-            location.href = "/register";
-        }
-    });
-});
