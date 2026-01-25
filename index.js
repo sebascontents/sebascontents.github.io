88f1fe71add8338e334da3e691d58d1f6aaed3b2
sebascontents <157534242+sebascontents@users.noreply.github.com>
2026-01-25 11:44:40 -0300

    Update index.js
    Signed-off-by: sebascontents <157534242+sebascontents@users.noreply.github.com>

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/api/login', (req, res) => {
     }
 });
 
-// --- 3. INICIO DEL SERVIDOR --- (Siempre al final)
-app.listen(5000, () => {
-    console.log("Servidor Backend corriendo en http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
